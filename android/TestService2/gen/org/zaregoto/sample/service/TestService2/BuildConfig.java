23ffe4f66216073013c36b2cc7fd7f3b6da08c3f
WAKABAYASHI,Ayumu <waka@zaregoto.org>
2015-09-16 15:17:21 +0900

    move junk codes to example folder

--- android/TestService2/gen/org/zaregoto/sample/service/TestService2/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.zaregoto.sample.service.TestService2;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android/TestService2/src/org/zaregoto/sample/service1/view/IObserver.aidl
+++
@@ -1,5 +0,0 @@
-package org.zaregoto.sample.service1.view;
-
-interface IObserver {
-    void onDataArrived(int data);
-}
