cfaa56d4a584270adeea3283ae561bcd783619e6
Ayumu WAKABAYASHI <waka@zaregoto.org>
2019-12-29 18:57:45 +0900

    add harib22h example initial

---
+++ osaskbook/day25/harib22h/hello3.c
@@ -0,0 +1,12 @@
+void _api_putchar(int c);
+void _api_end(void);
+
+void HariMain(void)
+{
+	_api_putchar('h');
+	_api_putchar('e');
+	_api_putchar('l');
+	_api_putchar('l');
+	_api_putchar('o');
+	_api_end();
+}
