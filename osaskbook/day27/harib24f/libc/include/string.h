a26d910991dbdeaf3d0e97ccc339e41e891ce90b
Ayumu WAKABAYASHI <waka@zaregoto.org>
2020-01-03 01:46:56 +0900

    add harib24fg example

---
+++ osaskbook/day27/harib24f/libc/include/string.h
@@ -0,0 +1,7 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int n);
+
+#endif /* __STRING_H__ */
---
+++ osaskbook/day27/harib24g/hello4/hello4.c
@@ -0,0 +1,8 @@
+void _api_putstr0(char *s);
+void _api_end(void);
+
+void HariMain(void)
+{
+	_api_putstr0("hello, world\n");
+	_api_end();
+}
