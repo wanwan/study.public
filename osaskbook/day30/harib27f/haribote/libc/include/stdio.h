bf568d85c208300e7634ce09077d8b84df9a4eea
Ayumu WAKABAYASHI <waka@zaregoto.org>
2020-01-05 21:32:55 +0900

    add harib27f example

---
+++ osaskbook/day30/harib27f/haribote/libc/include/stdio.h
@@ -0,0 +1,7 @@
+#ifndef __STDIO_H__
+#define __STDIO_H__
+
+
+void sprintf (char *str, char *fmt, ...);
+
+#endif /* __STDIO_H__ */
---
+++ osaskbook/day30/harib27f/libc/strcmp.c
@@ -0,0 +1,29 @@
+
+#define MAX_STR_CMP_LINE (64)
+
+int strcmp(char *str1, char *str2) {
+    int i;
+    for (i = 0; i < MAX_STR_CMP_LINE; i++) {
+        if (str1[i] == 0x00 && str2[i] == 0x00) return 0;
+        if (str1[i] == 0x00) return 1;
+        if (str2[i] == 0x00) return 1;
+        if (str1[i] != str2[i]) return 1;
+    }
+    return -1;
+}
+
+
+int strncmp(char *str1, char *str2, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+	if (i >= MAX_STR_CMP_LINE) {
+	    return 0;
+	}
+        if (str1[i] == 0x00 || str2[i] == 0x00) return 0;
+        if (str1[i] == 0x00) return 1;
+        if (str2[i] == 0x00) return 1;
+        if (str1[i] != str2[i]) return 1;	
+    }
+    return 0;
+}
+
