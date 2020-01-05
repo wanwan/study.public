c732838c3b4b92d63643cb6c9e0bca684456be7f
Ayumu WAKABAYASHI <waka@zaregoto.org>
2020-01-05 17:24:26 +0900

    add harib27b example (seems graphics memory broken?)

---
+++ osaskbook/day30/harib27b/libc/include/string.h
@@ -0,0 +1,9 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int n);
+
+int strlen(const char *str);
+
+#endif /* __STRING_H__ */
